51c45ce423f28afffb824315b4cc0597b5e65dc4
Onur <ob-vest@hotmail.com>
2021-12-16 23:19:26 +0100

    added about me

---
+++ slideshow.js
@@ -0,0 +1,16 @@
+// Source is https://www.w3schools.com/w3css/tryit.asp?filename=tryw3css_slideshow_rr
+
+var slideIndex = 0;
+carousel();
+
+function carousel() {
+  var i;
+  var x = document.getElementsByClassName("mySlides");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > x.length) {slideIndex = 1}
+  x[slideIndex-1].style.display = "block";
+  setTimeout(carousel, 6000); // Change image every x seconds
+}
