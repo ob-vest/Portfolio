d5411a7032dc81862ff822096717a2c17f4eddb2
Onur <ob-vest@hotmail.com>
2021-12-20 04:43:18 +0100

    added more projects and changed layout style with media queries

--- slideshow.js
+++ slideshow.js
@@ -12,5 +12,5 @@ function carousel() {
   slideIndex++;
   if (slideIndex > x.length) {slideIndex = 1}
   x[slideIndex-1].style.display = "block";
-  setTimeout(carousel, 6000); // Change image every x seconds
+  setTimeout(carousel, 8000); // Change image every x seconds
 }
